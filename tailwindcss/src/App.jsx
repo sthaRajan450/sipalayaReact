b6c873a3b616eaa3420809faf3359b59407591b0
rajan shrestha <stharajan477@gmail.com>
2025-05-02 17:47:32 +0545

    tailwind css

---
+++ tailwindcss/src/App.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+
+function App() {
+  return (
+    <div className="flex justify-between gap-3.5 my-20 mx-10 flex-wrap ">
+      {/* <h1 className=" text-xl h-[300px] text-center flex items-center border-4 shadow-2xl font-extrabold  delay-75 animate-bounce  shadow-blue-500 bg-black text-white/90 border-red-400 mx-10 my-20 rounded-full p-10 duration-150">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Vero, dicta?
+        Dolorem eligendi vel architecto labore rerum facere voluptate qui
+        quibusdam placeat quam impedit debitis repellat, distinctio perspiciatis
+        accusantium beatae ut?
+      </h1> */}
+
+
+
+      
+      <div className="flex justify-center items-center w-2xl h-[200px] rounded-full bg-red-400 text-black">hello-1</div>
+      <div className="flex justify-center items-center w-2xl h-[200px] rounded-full bg-red-400 text-black">hello-2</div>
+      <div className="flex justify-center items-center w-2xl h-[200px] rounded-full bg-red-400 text-black">hello-3</div>
+      <div className="flex justify-center items-center w-2xl h-[200px] rounded-full bg-red-400 text-black">hello-4</div>
+      <div className="flex justify-center items-center w-2xl h-[200px] rounded-full bg-red-400 text-black">hello-5</div>
+    </div>
+  );
+}
+
+export default App;
