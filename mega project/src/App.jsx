e1790430dfbefc0e41faa94912c947b8d0987ec9
rajan shrestha <stharajan477@gmail.com>
2025-05-02 08:08:08 +0545

    redux configured

--- mega project/src/App.jsx
+++ mega project/src/App.jsx
@@ -1,19 +1,45 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import conf from "./conf/conf";
-
+import authService from "./appwrite/auth";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./store/authSlice";
+import { Footer, Header } from "./components";
+import { Outlet } from "react-router-dom";
 
 const App = () => {
-  console.log(conf.appwriteUrl)
-  return (
-    <div className="w-full h-screen flex justify-center items-center">
-      <h1 className="bg-red-700">
-        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Temporibus,
-        odio ullam illum exercitationem nobis reiciendis minus repellendus
-        voluptatum atque, reprehenderit architecto consequuntur voluptas quis,
-        hic saepe sit voluptate! Possimus, placeat.
-      </h1>
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login({ userData }));
+        } else {
+          dispatch(logout());
+        }
+      })
+      .catch((error) => {
+        console.log("Failed to get current user", error);
+        dispatch(logout());
+      })
+      .finally(() => setLoading(false));
+  }, []);
+
+  return !loading ? (
+    <div>
+      <div className="min-h-screen flex flex-wrap bg-gray-400 content-center ">
+        <div className="w-full block">
+          <Header />
+          <main>
+            Todo:  <Outlet />
+          </main>
+          <Footer />
+        </div>
+      </div>
     </div>
-  );
+  ) : null;
 };
 
 export default App;
