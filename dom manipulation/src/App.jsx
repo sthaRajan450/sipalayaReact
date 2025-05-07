1410071729240b044e54bf0d5047b16ab32fc990
rajan shrestha <stharajan477@gmail.com>
2025-05-07 16:07:57 +0545

    useRef

---
+++ dom manipulation/src/App.jsx
@@ -0,0 +1,58 @@
+import React from "react";
+import { useRef } from "react";
+import { useEffect } from "react";
+import { useState } from "react";
+
+const App = () => {
+  // const [count, setCount] = useState(0);
+  // let val = useRef(0);
+  // let btnRef = useRef();
+  // useEffect(() => {
+  //   console.log("run on each render");
+  // });
+  const [time, setTime] = useState(0);
+  let timerRef = useRef(null);
+  function startTimer() {
+    timerRef.current = setInterval(() => {
+      setTime((time) => time + 1);
+    }, 1000);
+    console.log(timerRef.current);
+  }
+  function stopTimer() {
+    clearInterval(timerRef.current);
+    timerRef.current = null;
+  }
+  function resetTimer() {
+    stopTimer();
+    setTime(0);
+  }
+  return (
+    <div>
+      {/* <button
+        ref={btnRef}
+        onClick={() => {
+          val.current = val.current + 1;
+          console.log("value of vale is" + val.current);
+          setCount(count + 1);
+        }}
+      >
+        Increment
+      </button>
+      <p>Count:{count}</p>
+
+      <button
+        onClick={() => {
+          btnRef.current.style.backgroundColor = "red";
+        }}
+      >
+        change the color of button
+      </button> */}
+      <h1>Time: {time} seconds</h1>
+      <button onClick={startTimer}>Start</button>
+      <button onClick={stopTimer}>Stop</button>
+      <button onClick={resetTimer}>Reset</button>
+    </div>
+  );
+};
+
+export default App;
